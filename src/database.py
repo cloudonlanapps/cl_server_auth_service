f6e7cc5fece471f69fe7c24e1a2729e6554d4382
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-11-28 22:10:46 +0530

    disable query logging

--- src/database.py
+++ src/database.py
@@ -4,11 +4,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from .config import DATABASE_URL
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},  # Needed for SQLite
+    echo=False,  # Set to True for SQL query logging
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
